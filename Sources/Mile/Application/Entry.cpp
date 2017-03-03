b6f898fe4452084b0d3f8dcffb16f9229f6c36f5
Yang Kyowon <kyowonhorang@gmail.com>
2017-03-03 22:37:26 +0900

    Remove test codes

--- Sources/Mile/Application/Entry.cpp
+++ Sources/Mile/Application/Entry.cpp
@@ -18,18 +18,5 @@ int main( int Argc, char* Argv[], char* Envp[] )
     setlocale( LC_ALL, "" );
 
     /** @todo: Replace to Application end code */
-    SceneComponent* Comp1 = new SceneComponent( MString( TEXT( "SceneComp1" ) ) );
-    SceneComponent* Comp2 = new SceneComponent( MString( TEXT( "SceneComp2" ) ) );
-    Comp1->AttachTo( Comp2 );
-    Comp2->AttachTo( Comp1 );
-
-    Comp1->DetachFromComponent( );
-    Comp2->AttachTo( Comp1 );
-
-    Actor* TestActor = new Actor( MString( TEXT( "╬вем" ) ) );
-    TestActor->AttachComponent( Comp1 );
-    std::cout << TestActor->SetRootComponent( Comp1 ) << std::endl; 
-    TestActor->DetachComponent( Comp1 );
-    std::cout << TestActor->SetRootComponent( Comp1 ) << std::endl;
     return 0;
 }
