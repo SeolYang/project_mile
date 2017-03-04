6ce4488feaa8be1287e2d332bc565901bbebb7b1
Yang Kyowon <kyowonhorang@gmail.com>
2017-03-04 10:36:20 +0900

    Added SceneComponent Move constructor test

--- Sources/Mile/Application/Entry.cpp
+++ Sources/Mile/Application/Entry.cpp
@@ -17,6 +17,11 @@ int main( int Argc, char* Argv[], char* Envp[] )
 
     setlocale( LC_ALL, "" );
 
+    SceneComponent Comp1{ MString( TEXT( "Blah" ) ) };
+    SceneComponent Comp2{ std::move( Comp1 ) };
+    SceneComponent Comp3{ MString( TEXT( "Blah2" ) ) };
+    Comp3.AttachTo( &Comp1 );
+    Comp3.AttachTo( &Comp2 );
     /** @todo: Replace to Application end code */
     return 0;
 }
