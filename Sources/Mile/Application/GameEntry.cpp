4959b1face18e91140a9b6b062e471f615d44771
Yang Kyowon <kyowonhorang@gmail.com>
2017-03-23 09:32:40 +0900

    Added Game Entry

---
+++ Sources/Mile/Application/GameEntry.cpp
@@ -0,0 +1,23 @@
+#ifdef MILE_GAME
+#include "Application.h"
+#include "Win32Window.h"
+#include "TickManager.h"
+#include "Actor.h"
+using namespace Mile;
+
+#ifdef _DEBUG
+#define new new(_CLIENT_BLOCK, __FILE__, __LINE__)
+#endif
+
+int main( int Argc, char* Argv[ ], char* Envp[ ] )
+{
+#ifdef _DEBUG
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+    setlocale( LC_ALL, "" );
+
+    /** @todo: Replace to Application end code */
+    return 0;
+}
+#endif
