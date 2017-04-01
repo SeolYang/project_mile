ac369b6ed357a4e7fd3772c738cfd04b4e8c7095
Yang Kyowon <kyowonhorang@gmail.com>
2017-04-01 22:47:27 +0900

    InputSystem Key Mapping, Binding test

--- Sources/Mile/Application/GameEntry.cpp
+++ Sources/Mile/Application/GameEntry.cpp
@@ -2,12 +2,17 @@
 #include "Application.h"
 #include "Win32Window.h"
 #include "TickManager.h"
+#include "MileInputSystem.h"
 #include "Actor.h"
 using namespace Mile;
 
 #ifdef _DEBUG
 #define new new(_CLIENT_BLOCK, __FILE__, __LINE__)
 #endif
+
+void Test( )
+{
+}
 
 int main( int Argc, char* Argv[ ], char* Envp[ ] )
 {
@@ -17,7 +22,25 @@ int main( int Argc, char* Argv[ ], char* Envp[ ] )
 
     setlocale( LC_ALL, "" );
 
-    /** @todo: Replace to Application end code */
+    MString TestStr = MString( TEXT( "Act" ) );
+    InputSystem::Instance( ).MapAction( TestStr, SActionMappingProperty( EInputKey::IK_None ) );
+    InputSystem::Instance( ).BindAction( MString( TEXT( "Act" ) ), EActionInputEvent::IE_Any, Test );
+    std::cout << InputSystem::Instance( ).IsMappedAction( TestStr ) << std::endl;
+    std::cout << InputSystem::Instance( ).IsBindedAction( MString( TEXT( "Act" ) ), EActionInputEvent::IE_Any ) << std::endl;
+
+    InputSystem::Instance( ).UnbindAction( MString( TEXT( "Act" ) ), EActionInputEvent::IE_Pressed );
+    std::cout << InputSystem::Instance( ).IsBindedAction( MString( TEXT( "Act" ) ), EActionInputEvent::IE_Any ) << std::endl;
+    std::cout << InputSystem::Instance( ).IsBindedAction( MString( TEXT( "Act" ) ), EActionInputEvent::IE_Pressed ) << std::endl;
+    std::cout << InputSystem::Instance( ).IsBindedAction( MString( TEXT( "Act" ) ), EActionInputEvent::IE_Released ) << std::endl;
+
+    InputSystem::Instance( ).BindAction( TestStr, EActionInputEvent::IE_Pressed, Test );
+    std::cout << InputSystem::Instance( ).IsBindedAction( MString( TEXT( "Act" ) ), EActionInputEvent::IE_Any ) << std::endl;
+
+    InputSystem::Instance( ).UnmapAction( TestStr );
+    std::cout << InputSystem::Instance( ).IsMappedAction( TestStr ) << std::endl;
+    std::cout << InputSystem::Instance( ).IsBindedAction( MString( TEXT( "Act" ) ), EActionInputEvent::IE_Released ) << std::endl;
+
+    InputSystem::DestroyInstance( );
     return 0;
 }
 #endif
