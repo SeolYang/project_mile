025f28370ffa9035e86c0dbe41bee23bb7505948
Yang Kyowon <kyowonhorang@gmail.com>
2017-07-12 21:03:52 +0900

    Added Engine comment and World Initialize

--- Runtime/Core/Engine.cpp
+++ Runtime/Core/Engine.cpp
@@ -1,31 +1,41 @@
 #include "Engine.h"
 #include "Context.h"
+#include "World.h"
 
 namespace Mile
 {
     Engine::Engine( Context* context ) :
         SubSystem( context )
     {
+        auto allocator = m_context->GetAllocator( );
+
         m_context->RegisterSubSystem( this );
+
+        m_world = RUMIA_NEW( allocator, World, m_context );
+        m_context->RegisterSubSystem( m_world );
     }
 
     bool Engine::Initialize( )
     {
-        return false;
+        // Initialize subsystems
+        if ( !m_context->GetSubSystem<World>( )->Initialize( ) )
+        {
+            //@TODO: Add Log
+            return false;
+        }
+
+        return true;
     }
 
     void Engine::Update( )
     {
-
-    }
-
-    void Engine::PostUpdate( )
-    {
-
+        // Update subsystems
+        m_world->Update( );
     }
 
     void Engine::ShutDown( )
     {
-
+        m_world = nullptr;
+        RUMIA_DELETE( m_context->GetAllocator( ), m_context );
     }
 }
--- Runtime/Core/Engine.h
+++ Runtime/Core/Engine.h
@@ -4,18 +4,31 @@
 
 namespace Mile
 {
+    class World;
     class MEAPI Engine : public SubSystem
     {
     public:
         Engine( Context* context );
         virtual ~Engine( ) { ShutDown( ); }
 
+        /**
+        * @brief Engine의 Subsystem 들을 초기화합니다.
+        * @return 초기화 성공 여부
+        */
         virtual bool Initialize( ) override;
 
+        /**
+        * @brief Engine의 Subsystem 들을 업데이트합니다.
+        */
         void Update( );
-        void PostUpdate( );
 
+        /**
+        * @brief Engine의 Subsystem 들을 해제합니다.
+        */
         void ShutDown( );
+
+    private:
+        World*  m_world;
 
     };
 }
