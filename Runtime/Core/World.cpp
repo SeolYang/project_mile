07654172480ea7743640fde78ad471669fea127a
Yang Kyowon <kyowonhorang@gmail.com>
2017-07-12 21:03:05 +0900

    Implement world

---
+++ Runtime/Core/World.cpp
@@ -0,0 +1,70 @@
+#include "World.h"
+#include "Context.h"
+#include "Entity.h"
+
+namespace Mile
+{
+    World::World( Context* context ) :
+        SubSystem( context ),
+        m_entities( context->GetAllocator( ) )
+    {
+
+    }
+
+    World::~World( )
+    {
+    }
+
+    bool World::Initialize( )
+    {
+        return false;
+    }
+
+    void World::Start( )
+    {
+        for ( auto entity : m_entities )
+        {
+            entity->Start( );
+        }
+    }
+
+    void World::Update( )
+    {
+        for ( auto entity : m_entities )
+        {
+            entity->Update( );
+        }
+    }
+
+    WeakEntity World::CreateEntity( )
+    {
+        auto newEntity = RUMIA_MAKE_SHARED( m_context->GetAllocator( ), Entity, m_context );
+        m_entities.PushBack( newEntity );
+
+        return newEntity;
+    }
+
+    WeakEntity World::GetEntityByName( const std::string& name )
+    {
+        for ( const auto& entity : m_entities )
+        {
+            if ( name == ( entity )->GetName( ) )
+            {
+                return entity;
+            }
+        }
+
+        return WeakEntity( );
+    }
+
+    Array<WeakEntity> World::GetEntities( )
+    {
+        Array<WeakEntity> tempEntities( m_context->GetAllocator( ) );
+        for ( const auto& entity : m_entities )
+        {
+            tempEntities.PushBack( entity );
+        }
+
+        return std::move( tempEntities );
+    }
+}
--- Runtime/Core/World.h
+++ Runtime/Core/World.h
@@ -1,11 +1,56 @@
 #pragma once
 
 #include "SubSystem.h"
+#include "Rumia/Array.h"
 
 namespace Mile
 {
+    class Entity;
+    using WeakEntity = std::weak_ptr<Entity>;
+    using SharedEntity = std::shared_ptr<Entity>;
+
     class MEAPI World : public SubSystem
     {
+    public:
         World( Context* context );
+        virtual ~World( );
+
+        /**
+        * @brief    World를 초기화합니다.
+        */
+        virtual bool Initialize( ) override;
+
+        /**
+        * @brief    World에 생성되어있는 Entity들의 Start 함수를 호출합니다.
+        */
+        void Start( );
+
+        /**
+        * @brief    World에 생성되어있는 Entity들을 Update 합니다.
+        */
+        void Update( );
+
+        /**
+        * @brief    World에 새로운 Entity를 생성합니다.
+        * @return   생성된 Entity
+        */
+        WeakEntity CreateEntity( );
+
+        /**
+        * @brief    World에 생성되어있는 Entitiy중 주어진 이름을 가진 Entity를 반환합니다.
+        *           ( 단 같은 이름을 가지는 Entity중 제일 먼저 추가된 Entity를 반환함. )
+        * @param    해당하는 이름을 가지는 Entity
+        */
+        WeakEntity GetEntityByName( const std::string& name );
+
+        /**
+        * @brief    World에 생성어있는 모든 Entity들의 배열을 반환합니다.
+        * @param    Entity 배열
+        */
+        Array<WeakEntity> GetEntities( );
+
+    private:
+        Array<SharedEntity> m_entities;
+
     };
 }
