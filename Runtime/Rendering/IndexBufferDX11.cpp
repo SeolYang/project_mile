62f80787338d3cf475022490eb29857abcfe4850
Yang Kyowon <kyowonhorang@gmail.com>
2017-11-07 11:08:35 +0900

    Added IndexBufferDX11

--- Runtime/Rendering/IndexBufferDX11.cpp
+++ Runtime/Rendering/IndexBufferDX11.cpp
@@ -1,9 +1,57 @@
 #include "IndexBufferDX11.h"
+#include "RendererDX11.h"
 
 namespace Mile
 {
    bool IndexBufferDX11::Init( const std::vector<unsigned int>& indicies, bool dynamic )
    {
+      if ( !m_bIsInitialized )
+      { 
+         return false;
+      }
+
+      D3D11_BUFFER_DESC desc;
+      
+      ZeroMemory( &desc, sizeof( desc ) );
+      desc.ByteWidth = sizeof( unsigned int ) * indicies.size( );
+      desc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+      desc.MiscFlags = 0;
+      
+      if ( dynamic )
+      {
+         desc.Usage = D3D11_USAGE_DYNAMIC;
+         desc.CPUAccessFlags = true;
+      }
+      else
+      {
+         desc.Usage = D3D11_USAGE_IMMUTABLE;
+         desc.CPUAccessFlags = false;
+      }
+
+      D3D11_SUBRESOURCE_DATA subResource;
+      ZeroMemory( &subResource, sizeof( subResource ) );
+      subResource.pSysMem = indicies.data( );
+
+      auto result = m_renderer->GetDevice( )->CreateBuffer( &desc, &subResource, &m_buffer );
+      if ( FAILED( result ) )
+      {
+         return false;
+      }
+
+      m_bIsInitialized = true;
+      m_desc = desc;
       return true;
+   }
+
+   bool IndexBufferDX11::Bind( )
+   {
+      if ( m_bIsInitialized )
+      {
+         auto deviceContext = m_renderer->GetDeviceContext( );
+         deviceContext->IASetIndexBuffer( m_buffer, DXGI_FORMAT_R32_UINT, 0 );
+         return true;
+      }
+
+      return false;
    }
 }
--- Runtime/Rendering/IndexBufferDX11.h
+++ Runtime/Rendering/IndexBufferDX11.h
@@ -6,9 +6,15 @@ namespace Mile
    class MEAPI IndexBufferDX11 : public BufferDX11
    {
    public:
-      IndexBufferDX11( ) { }
+      IndexBufferDX11( RendererDX11* renderer ) :
+         BufferDX11( renderer )
+      { 
+      }
 
       bool Init( const std::vector<unsigned int>& indicies, bool dynamic = false );
+      bool Bind( );
+
+      virtual RenderResourceType GetResourceType( ) const override { return RenderResourceType::RDRT_IndexBuffer; }
       
    };
 }
