d59ff86c59b9970ea186883ecc7a4a92dfb1e31c
Yang Kyowon <kyowonhorang@gmail.com>
2017-11-24 17:44:19 +0900

    Added MeshRender Component

---
+++ Runtime/Component/MeshRenderComponent.cpp
@@ -0,0 +1,43 @@
+#include "MeshRenderComponent.h"
+#include "Core\Entity.h"
+#include "Core\Context.h"
+#include "Resource\ResourceManager.h"
+#include "Resource\Model.h"
+#include "Resource\Material.h"
+#include "Rendering\Mesh.h"
+
+namespace Mile
+{
+   std::string MeshRenderComponent::Serialize( ) const
+   {
+      //auto model = m_mesh->Ge
+      // { "Mesh": { "Model": (Model filepath), "Name": (Mesh Name) },
+      // "Material": (Material filepath) }
+      //std::string res = "{ \"Mesh\": " + 
+      std::string res = "{ \"Mesh\": { \"Model\": \"" + WString2String( m_mesh->GetModelPath( ) ) + 
+         "\", \"Name\": \"" + m_mesh->GetName( ) + "\" }, "
+         " \"Material\": \"" + WString2String( m_material._Get( )->GetPath( ) ) + "\" }";
+      return res;
+   }
+
+   void MeshRenderComponent::DeSerialize( const json& jsonData )
+   {
+      auto resMng = m_entity->GetContext( )->GetSubSystem<ResourceManager>( );
+      json meshData = jsonData[ "Mesh" ];
+      auto loadedModel = resMng->Load<Model>( String2WString( meshData[ "Model" ] ) );
+      if ( loadedModel.expired( ) )
+      {
+         return;
+      }
+      m_mesh = loadedModel._Get( )->GetMeshByName( meshData[ "Name" ] );
+
+      m_material = resMng->Load<Material>( String2WString( jsonData[ "Material" ] ) );
+      // Begin
+      //json meshData = jsonData[ "Mesh" ];
+      //meshData["Model"] -> Resource Load
+      // m_mesh = LoadedModel->GetMeshByName( meshData["Name"] )
+      //jsonData[ "Material" ] -> Resource Load
+      // m_material = LoadedMaterial
+      // End
+   }
+}
---
+++ Runtime/Component/MeshRenderComponent.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Component.h"
+
+namespace Mile
+{
+   class Material;
+   class Mesh;
+   class MEAPI MeshRenderComponent : public Component
+   {
+   public:
+      MeshRenderComponent( Entity* entity ) : 
+         m_mesh( nullptr ),
+         Component( entity )
+      {
+      }
+
+      virtual std::string Serialize( ) const override;
+      virtual void DeSerialize( const json& jsonData ) override;
+
+      void SetMesh( Mesh* mesh ) { m_mesh = mesh; }
+      Mesh* GetMesh( ) const { return m_mesh; }
+
+      void SetMaterial( std::weak_ptr<Material> material ) { m_material = material; }
+      std::weak_ptr<Material> GetMaterial( ) const { return m_material; }
+
+   private:
+      Mesh*                   m_mesh;
+      std::weak_ptr<Material> m_material;
+
+   };
+}
