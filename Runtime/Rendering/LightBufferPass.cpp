4ee940b4fd105786a0ee4b86083a3fa632b0a82e
Yang Kyowon <kyowonhorang@gmail.com>
2017-11-28 11:42:54 +0900

    Added LightBufferPass

---
+++ Runtime/Rendering/LightBufferPass.cpp
@@ -0,0 +1,132 @@
+#include "LightBufferPass.h"
+#include "ConstantBufferDX11.h"
+#include "Texture2dDX11.h"
+#include "GBuffer.h"
+#include "RenderTargetDX11.h"
+
+namespace Mile
+{
+   LightBufferPass::LightBufferPass( RendererDX11* renderer ) :
+      m_lightBuffer( nullptr ),
+      m_cameraBuffer( nullptr ),
+      m_gBuffer( nullptr ),
+      RenderingPass( renderer )
+   {
+   }
+
+   LightBufferPass::~LightBufferPass( )
+   {
+      SafeDelete( m_lightParamBuffer );
+      SafeDelete( m_cameraBuffer );
+   }
+
+   bool LightBufferPass::Init( const String& filePath )
+   {
+      bool bIsInitialized = m_lightParamBuffer != nullptr || m_cameraBuffer != nullptr;
+      if ( m_renderer == nullptr || bIsInitialized )
+      {
+         return false;
+      }
+
+      m_lightParamBuffer = new ConstantBufferDX11( m_renderer );
+      if ( !m_lightParamBuffer->Init( sizeof( LightParamConstantBuffer ) ) )
+      {
+         return false;
+      }
+
+      m_cameraBuffer = new ConstantBufferDX11( m_renderer );
+      if ( !m_cameraBuffer->Init( sizeof( CameraConstantBuffer ) ) )
+      {
+         return false;
+      }
+
+      return true;
+   }
+
+   bool LightBufferPass::Bind( )
+   {
+      if ( !RenderingPass::Bind( ) )
+      {
+         return false;
+      }
+
+      bool bIsInitialized = m_lightBuffer != nullptr && m_cameraBuffer != nullptr;
+      if ( !bIsInitialized || m_gBuffer == nullptr || m_lightBuffer == nullptr )
+      {
+         return false;
+      }
+
+      if ( !m_lightBuffer->BindAsRenderTarget( ) )
+      {
+         return false;
+      }
+
+      if ( !m_gBuffer->BindAsShaderResource( 0 ) )
+      {
+         return false;
+      }
+
+      if ( !m_lightParamBuffer->Bind( 0, ShaderType::PixelShader ) )
+      {
+         return false;
+      }
+
+      if ( !m_cameraBuffer->Bind( 1, ShaderType::PixelShader ) )
+      {
+         return false;
+      }
+
+      return true;
+   }
+
+   void LightBufferPass::Unbind( )
+   {
+      if ( m_renderer == nullptr )
+      {
+         return;
+      }
+
+      RenderingPass::Unbind( );
+      m_gBuffer->Unbind( );
+   }
+
+   void LightBufferPass::SetGBuffer( GBuffer* gBuffer )
+   {
+      m_gBuffer = gBuffer;
+   }
+
+   void LightBufferPass::SetLightBuffer( RenderTargetDX11* lightBuffer )
+   {
+      m_lightBuffer = lightBuffer;
+   }
+
+   void LightBufferPass::UpdateLightParamBuffer( const Vector3& lightPos,
+                                            const Vector3& lightColor,
+                                            const Vector3& lightDirection,
+                                            const Vector2& spotlightAngles,
+                                            const Vector4& lightRange )
+   {
+      if ( m_lightBuffer != nullptr )
+      {
+         auto buffer = reinterpret_cast< LightParamConstantBuffer* >( m_lightBuffer->Map( ) );
+         buffer->LightPos = lightPos;
+         buffer->LightColor = lightColor;
+         buffer->LightDirection = lightDirection;
+         buffer->SpotlightAngles = spotlightAngles;
+         buffer->LightRange = lightRange;
+
+         m_lightParamBuffer->UnMap( );
+      }
+   }
+
+   void LightBufferPass::UpdateCameraBuffer( const Vector3& camPos )
+   {
+      if ( m_cameraBuffer != nullptr )
+      {
+         auto buffer = reinterpret_cast< CameraConstantBuffer* >( m_cameraBuffer->Map( ) );
+         buffer->CameraPos = camPos;
+
+         m_cameraBuffer->UnMap( );
+      }
+   }
+}
---
+++ Runtime/Rendering/LightBufferPass.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "RenderingPass.h"
+#include "Math\Vector2.h"
+#include "Math\Vector3.h"
+#include "Math\Vector4.h"
+
+namespace Mile
+{
+   class GBuffer;
+   class MEAPI LightBufferPass : public RenderingPass
+   {
+      struct LightParamConstantBuffer
+      {
+         Vector3 LightPos;
+         Vector3 LightColor;
+         Vector3 LightDirection;
+         Vector2 SpotlightAngles;
+         Vector4 LightRange;
+      };
+
+      struct CameraConstantBuffer
+      {
+         Vector3 CameraPos;
+      };
+
+   public:
+      LightBufferPass( RendererDX11* renderer );
+      ~LightBufferPass( );
+
+      virtual bool Init( const String& filePath );
+      bool Bind( );
+      virtual void Unbind( ) override;
+
+      void SetGBuffer( GBuffer* gBuffer );
+      void SetLightBuffer( RenderTargetDX11* lightBuffer );
+
+      void UpdateLightParamBuffer( const Vector3& lightPos, 
+                              const Vector3& lightColor,
+                              const Vector3& lightDirection,
+                              const Vector2& spotlightAngles,
+                              const Vector4& lightRange );
+
+      void UpdateCameraBuffer( const Vector3& camPos );
+
+   private:
+      ConstantBufferDX11*  m_lightParamBuffer;
+      ConstantBufferDX11*  m_cameraBuffer;
+      
+      GBuffer*             m_gBuffer;
+      RenderTargetDX11*    m_lightBuffer;
+
+   };
+}
