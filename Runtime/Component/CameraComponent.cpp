177e73ec87328c9ceae1ea59c5fda7560b035868
Yang Kyowon <kyowonhorang@gmail.com>
2017-11-30 11:19:39 +0900

    Added CameraComponent

---
+++ Runtime/Component/CameraComponent.cpp
@@ -0,0 +1,13 @@
+#include "CameraComponent.h"
+
+namespace Mile
+{
+   CameraComponent::CameraComponent( Entity* entity ) :
+      m_fov( 45.0f ),
+      m_nearPlane( 1.0f ),
+      m_farPlane( 500.0f ),
+      m_clearColor( Vector4( 0.0f, 0.0f, 0.0f, 1.0f ) ),
+      Component( entity )
+   {
+   }
+}
---
+++ Runtime/Component/CameraComponent.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Component.h"
+#include "Math\Vector4.h"
+
+namespace Mile
+{
+   class MEAPI CameraComponent : public Component
+   {
+   public:
+      CameraComponent( Entity* entity );
+
+      float GetFov( ) const { return m_fov; }
+      void SetFov( float fov ) { m_fov = fov; }
+
+      float GetNearPlane( ) const { return m_nearPlane; }
+      void SetNearPlane( float nearPlane ) { m_nearPlane = nearPlane; }
+
+      float GetFarPlane( ) const { return m_farPlane; }
+      void SetFarPlane( float farPlane ) { m_farPlane = farPlane; }
+
+      Vector4 GetClearColor( ) const { return m_clearColor; }
+      void SetClearColor( const Vector4& clearColor ) { m_clearColor = clearColor; }
+
+   private:
+      float m_fov;
+      float m_nearPlane;
+      float m_farPlane;
+
+      Vector4 m_clearColor;
+
+   };
+}
