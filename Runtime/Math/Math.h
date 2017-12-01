b49efee017083adc63c47b0e1628796fa57402a1
Yang Kyowon <kyowonhorang@gmail.com>
2017-12-01 17:22:53 +0900

    Added some math helper functions ( rad2deg, deg2rad)

--- Runtime/Math/Math.h
+++ Runtime/Math/Math.h
@@ -6,5 +6,16 @@ namespace Mile
    namespace Math
    {
       static const Float Pi = (Float)3.14159265;
+
+      static float DegreeToRadian( float degree )
+      {
+         return (degree / 180.0f) * Pi;
+      }
+
+      static float RadianToDegree( float radian )
+      {
+         return ( radian / Pi ) * 180.0f;
+      }
+
    }
 }
