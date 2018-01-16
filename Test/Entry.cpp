37b8128c335f8cb6de09b27280f70b30487c21c2
Yang Kyowon <kyowonhorang@gmail.com>
2018-01-16 22:06:48 +0900

    Fxied Component Register

--- Test/Entry.cpp
+++ Test/Entry.cpp
@@ -1,5 +1,15 @@
 #include "Core\Context.h"
 #include "Core\Engine.h"
+#include "Core\World.h"
+#include "Core\Entity.h"
+#include "Component\CameraComponent.h"
+#include "Component\MeshRenderComponent.h"
+#include "Component\LightComponent.h"
+#include "Resource\ResourceManager.h"
+#include "Resource\Model.h"
+#include "Resource\ModelLoader.h"
+#include "Resource\Material.h"
+#include "Resource\Texture2D.h"
 
 using namespace Mile;
 
@@ -13,7 +23,32 @@ int main( )
       return 1;
    }
 
+   auto world = context->GetSubSystem<World>( );
+   auto resMng = context->GetSubSystem<ResourceManager>( );
+
+   auto camera = world->CreateEntity( TEXT( "Camera" ) );
+   auto camComp = camera->AddComponent<CameraComponent>( );
+   auto camTransform = camera->GetTransform( );
+   camTransform->SetPosition( Vector3( 0.0f, 0.0f, -100.0f ) );
+
+   // Camera setup
+
+   auto light = world->CreateEntity( TEXT( "DirectionalLight" ) );
+   auto lightComp = camera->AddComponent<LightComponent>( );
+
+   // Light setup
+
+   auto model = resMng->Load<Model>( TEXT( "Contents/Models/dragon.obj" ) );
+   auto modelEntity = Model::Instantiate( model, world );
+   auto modelMeshRenderer = modelEntity->GetComponent<MeshRenderComponent>( );
+   auto mat = modelMeshRenderer->GetMaterial( );
+
+   auto texture = resMng->Load<Texture2D>( TEXT( "Contents/Textures/test.png" ) );
+   mat->SetDiffuseMap( texture );
+
+   // Model setup
+
    int execute = engine->Execute( );
-   context.reset( );
+   SafeDelete( engine );
    return execute;
 }
