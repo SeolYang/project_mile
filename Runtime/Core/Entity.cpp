a2b8941ba6868dd2ef2f838a8bee9108281df588
Yang Kyowon <kyowonhorang@gmail.com>
2019-12-15 18:52:31 +0900

    Added log for invalid serialized entity data
    And fixed wrong pointer referencing from entity.

--- Runtime/Core/Entity.cpp
+++ Runtime/Core/Entity.cpp
@@ -1,236 +1,223 @@
 #include "Entity.h"
 #include "Context.h"
+#include "Logger.h"
 #include "Component/Component.h"
 #include "ComponentRegister.h"
 #include "World.h"
 
 namespace Mile
 {
-   Entity::Entity( World* world, const String& name ) :
-      m_world( world ),
-      m_context( nullptr ),
-      m_name( name ),
-      m_bIsActive( true ),
-      m_transform( new Transform( this ) ),
-      m_parent( nullptr )
-   {
-      if ( m_world != nullptr )
-      {
-         m_context = m_world->GetContext( );
-      }
-   }
+	Entity::Entity(World* world, const String& name) :
+		m_world(world),
+		m_context(nullptr),
+		m_name(name),
+		m_bIsActive(true),
+		m_transform(new Transform(this)),
+		m_parent(nullptr)
+	{
+		if (m_world != nullptr)
+		{
+			m_context = m_world->GetContext();
+		}
+	}
 
-   Entity::~Entity( )
-   {
-      SafeDelete( m_transform );
-      for ( auto component : m_components )
-      {
-         SafeDelete( component );
-      }
-      for ( auto child : m_children )
-      {
-         SafeDelete( child );
-      }
-   }
+	Entity::~Entity()
+	{
+		SafeDelete(m_transform);
+		for (auto component : m_components)
+		{
+			SafeDelete(component);
+		}
+		for (auto child : m_children)
+		{
+			SafeDelete(child);
+		}
+	}
 
-   json Entity::Serialize() const
-   {
-	   json serialized;
-	   serialized["Name"] = Mile::WString2String(m_name);
-	   serialized["IsActivated"] = m_bIsActive;
-	   serialized["Transform"] = m_transform->Serialize();
+	json Entity::Serialize() const
+	{
+		json serialized;
+		serialized["Name"] = Mile::WString2String(m_name);
+		serialized["IsActivated"] = m_bIsActive;
+		serialized["Transform"] = m_transform->Serialize();
 
-	   std::vector<json> components;
-	   for (auto comp : m_components)
-	   {
-		   components.push_back(comp->Serialize());
-	   }
-	   serialized["Components"] = components;
+		std::vector<json> components;
+		for (auto comp : m_components)
+		{
+			components.push_back(comp->Serialize());
+		}
+		serialized["Components"] = components;
 
-	   std::vector<json> children;
-	   for (auto child : m_children)
-	   {
-		   children.push_back(child->Serialize());
-	   }
-	   serialized["Children"] = children;
+		std::vector<json> children;
+		for (auto child : m_children)
+		{
+			children.push_back(child->Serialize());
+		}
+		serialized["Children"] = children;
 
-	   return serialized;
-   }
+		return serialized;
+	}
 
-   //std::string Entity::Serialize( ) const
-   //{
-   //   std::string res = "{ \"Name\": \"" + Mile::WString2String(m_name) +"\", \"IsActivated\": " + BoolSerialize( m_bIsActive ) +
-   //      ", \"Transform\": " + m_transform->Serialize( ) + ", \"Components\": [  ";
-   //   for ( auto comp : m_components )
-   //   {
-   //      res += comp->Serialize( ) + ", ";
-   //   }
-   //   res[ res.length( ) - 2 ] = ']';
+	void Entity::DeSerialize(const json& jsonData)
+	{
+		m_name = Mile::String2WString(jsonData["Name"]);
+		m_bIsActive = jsonData["IsActivated"];
 
-   //   res += ", \"Children\": [  ";
-   //   for ( auto child : m_children )
-   //   {
-   //      res += child->Serialize( ) + ", ";
-   //   }
-   //   res[ res.length( ) - 2 ] = ']';
+		std::vector<json> components = jsonData["Components"];
+		for (auto component : components)
+		{
+			auto foundedType = component.find("Type");
+			if (foundedType == component.end())
+			{
+				MELog(this->GetContext(),
+					TEXT("Entity"), ELogType::FATAL,
+					TEXT("Can't findout any type information from serialized data!"),
+					true);
+			}
 
-   //   res += '}';
+			std::string type = (*foundedType);
+			Component* compInst = ComponentRegister::GetInstance().Acquire(type, this);
+			compInst->DeSerialize(component);
+			m_components.push_back(compInst);
+		}
 
-   //   return res;
-   //}
+		std::vector<json> children = jsonData["Children"];
+		for (auto child : children)
+		{
+			Entity* tempChild = m_world->CreateEntity(TEXT(""));
+			tempChild->DeSerialize(child);
+			this->AttachChild(tempChild);
+		}
 
-   void Entity::DeSerialize( const json& jsonData )
-   {
-      m_name = Mile::String2WString( jsonData[ "Name" ] );
-      m_bIsActive = jsonData[ "IsActivated" ];
+		m_transform->DeSerialize(jsonData["Transform"]);
+	}
 
-      std::vector<json> components = jsonData[ "Components" ];
-      for ( auto component : components )
-      {
-         std::string type = component[ "Type" ];
-         Component* compInst = ComponentRegister::GetInstance( ).Acquire( type, this );
-         compInst->DeSerialize( component );
-         m_components.push_back( compInst );
-      }
+	bool Entity::Init()
+	{
+		return true;
+	}
 
-      std::vector<json> children = jsonData[ "Children" ];
-      for ( auto child : children )
-      {
-         Entity* tempChild = m_world->CreateEntity( TEXT( "" ) );
-         tempChild->DeSerialize( child );
-         this->AttachChild( tempChild );
-      }
+	void Entity::SetActive(bool bIsActive)
+	{
+		if (m_bIsActive != bIsActive)
+		{
+			m_bIsActive = bIsActive;
+			if (m_bIsActive)
+			{
+				OnEnable();
+			}
+			else
+			{
+				OnDisable();
+			}
+		}
+	}
 
-	  m_transform->DeSerialize(jsonData["Transform"]);
-   }
+	bool Entity::AttachChild(Entity* child)
+	{
+		if (child != nullptr)
+		{
+			if (child != this && child->m_parent != this)
+			{
+				Entity* oldParent = child->m_parent;
+				if (oldParent != nullptr)
+				{
+					if (!oldParent->DetachChild(child))
+					{
+						return false;
+					}
+				}
 
-   bool Entity::Init( )
-   {
-      return true;
-   }
+				child->m_parent = this;
+				child->GetTransform()->SetParent(m_transform);
+				m_children.push_back(child);
+				return true;
+			}
+		}
 
-   void Entity::SetActive( bool bIsActive )
-   {
-      if ( m_bIsActive != bIsActive )
-      {
-         m_bIsActive = bIsActive;
-         if ( m_bIsActive )
-         {
-            OnEnable( );
-         }
-         else
-         {
-            OnDisable( );
-         }
-      }
-   }
+		return false;
+	}
 
-   bool Entity::AttachChild( Entity* child )
-   {
-      bool bIsNotValidPtr = child == nullptr || child == this;
-      if (bIsNotValidPtr)
-      {
-		  if (child->m_parent == this)
-		  {
-			  return false;
-		  }
-      }
+	bool Entity::DetachChild(Entity* child)
+	{
+		bool bIsValidChild = child != nullptr && child != this;
+		if (bIsValidChild)
+		{
+			if (child->m_parent == this)
+			{
+				m_children.erase(std::find(m_children.begin(), m_children.end(), child));
+				child->GetTransform()->SetParent(nullptr);
+				return true;
+			}
+		}
 
-      Entity* oldParent = child->m_parent;
-      if ( oldParent != nullptr )
-      {
-         if ( !oldParent->DetachChild( child ) )
-         {
-            return false;
-         }
-      }
+		return false;
+	}
 
-      child->m_parent = this;
-      child->GetTransform( )->SetParent( m_transform );
-      m_children.push_back( child );
-      return true;
-   }
+	void Entity::SetTransform(const Transform& transform)
+	{
+		(*m_transform) = transform;
+	}
 
-   bool Entity::DetachChild( Entity* child )
-   {
-      bool bIsNotValidPtr = child != nullptr && child != this;
-	  if (bIsNotValidPtr)
-	  {
-		  if (child->m_parent == this)
-		  {
-			  return false;
-		  }
-	  }
+	Entity* Entity::GetChildByName(const String& name)
+	{
+		for (auto child : m_children)
+		{
+			if (child->GetName() == name)
+			{
+				return child;
+			}
+		}
 
-      m_children.erase( std::find( m_children.begin( ), m_children.end( ), child ) );
-      child->GetTransform( )->SetParent( nullptr );
-      return true;
-   }
+		return nullptr;
+	}
 
-   void Entity::SetTransform( const Transform& transform )
-   {
-      ( *m_transform ) = transform;
-   }
+	void Entity::Start()
+	{
+		for (auto component : m_components)
+		{
+			component->Start();
+		}
+	}
 
-   Entity* Entity::GetChildByName( const String& name )
-   {
-      for ( auto child : m_children )
-      {
-         if ( child->GetName( ) == name )
-         {
-            return child;
-         }
-      }
+	void Entity::Update()
+	{
+		if (m_bIsActive)
+		{
+			for (auto component : m_components)
+			{
+				if (component->IsActive())
+				{
+					component->Update();
+				}
+			}
 
-      return nullptr;
-   }
+			for (auto child : m_children)
+			{
+				child->Update();
+			}
+		}
+	}
 
-   void Entity::Start( )
-   {
-      for ( auto component : m_components )
-      {
-         component->Start( );
-      }
-   }
+	void Entity::OnEnable()
+	{
+		for (auto component : m_components)
+		{
+			if (component->IsActive())
+			{
+				component->OnEnable();
+			}
+		}
+	}
 
-   void Entity::Update( )
-   {
-      if ( m_bIsActive )
-      {
-         for ( auto component : m_components )
-         {
-            if ( component->IsActive( ) )
-            {
-               component->Update( );
-            }
-         }
-
-         for ( auto child : m_children )
-         {
-            child->Update( );
-         }
-      }
-   }
-
-   void Entity::OnEnable( )
-   {
-      for ( auto component : m_components )
-      {
-         if ( component->IsActive( ) )
-         {
-            component->OnEnable( );
-         }
-      }
-   }
-
-   void Entity::OnDisable( )
-   {
-      for ( auto component : m_components )
-      {
-         if ( component->IsActive( ) )
-         {
-            component->OnDisable( );
-         }
-      }
-   }
+	void Entity::OnDisable()
+	{
+		for (auto component : m_components)
+		{
+			if (component->IsActive())
+			{
+				component->OnDisable();
+			}
+		}
+	}
 }
