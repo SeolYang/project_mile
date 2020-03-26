6a2fac6e9b42f8368b33b6301c465db37ffe1f3e
Yang Kyowon <kyowonhorang@gmail.com>
2020-03-27 04:14:34 +0900

    Flip UV of Quad

--- Runtime/Rendering/Quad.cpp
+++ Runtime/Rendering/Quad.cpp
@@ -8,10 +8,10 @@ namespace Mile
       float maxX, float maxY)
    {
       std::vector<VertexPosTex> vertices = {
-      { Vector4(minX, maxY, 0.0f, 1.0f), Vector2(0.0f, 1.0f) },
-      { Vector4(maxX, maxY, 0.0f, 1.0f), Vector2(1.0f, 1.0f) },
-      { Vector4(minX, minY, 0.0f, 1.0f), Vector2(0.0f, 0.0f) },
-      { Vector4(maxX, minY, 0.0f, 1.0f), Vector2(1.0f, 0.0f) } };
+      { Vector4(minX, maxY, 0.0f, 1.0f), Vector2(0.0f, 0.0f) },
+      { Vector4(maxX, maxY, 0.0f, 1.0f), Vector2(1.0f, 0.0f) },
+      { Vector4(minX, minY, 0.0f, 1.0f), Vector2(0.0f, 1.0f) },
+      { Vector4(maxX, minY, 0.0f, 1.0f), Vector2(1.0f, 1.0f) } };
 
       std::vector<unsigned int> indices = {
          0, 1, 2,
