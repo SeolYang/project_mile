3f9f5ffba39af39104ef8842aa31b4942c37f802
Yang Kyowon <kyowonhorang@gmail.com>
2020-03-27 04:20:39 +0900

    New example for PBS workflow

--- Test/Entry.cpp
+++ Test/Entry.cpp
@@ -30,101 +30,45 @@ int main( )
    auto world = context->GetSubSystem<World>( );
    auto resMng = context->GetSubSystem<ResourceManager>( );
 
-   //auto camera = world->CreateEntity( TEXT( "Camera" ) );
-   //auto camComp = camera->AddComponent<CameraComponent>( );
-   //auto camTransform = camera->GetTransform( );
-   //camTransform->SetPosition( Vector3( 0.0f, 0.0f, -180.0f ) );
-   ////camTransform->SetRotation( Quaternion( 45.0f, Vector3::Up( ) ) );
+   Entity* camera = world->CreateEntity( TEXT( "Camera" ) );
+   CameraComponent* camComponent = camera->AddComponent<CameraComponent>( );
+   Transform* camTransform = camera->GetTransform( );
+   camComponent->SetNearPlane(1.0f);
+   camComponent->SetFarPlane(1000.0f);
+   camComponent->SetFov(60.0f);
+   camTransform->SetPosition(Vector3(0.0f, 0.0f, -5.0f));
 
-   //camComp->SetNearPlane( 1.0f );
-   //camComp->SetFarPlane( 1000.0f );
-   //camComp->SetFov( 60.0f );
+   Entity* upperLight = world->CreateEntity(TEXT("Upper Light"));
+   LightComponent* upperLightComponent = upperLight->AddComponent<LightComponent>();
+   Transform* upperLightTransform = upperLight->GetTransform();
+   upperLightComponent->SetLightType(ELightType::Point);
+   upperLightComponent->SetRadiance(Vector3(100.0f, 100.0f, 100.0f));
+   upperLightTransform->SetPosition(Vector3(0.0f, 10.0f, 0.0f));
 
-   ////camera->AddComponent<RotateComponent>( );
-   //// Camera setup
+   Entity* leftLight = world->CreateEntity(TEXT("Left Light"));
+   LightComponent* leftLightComponent = leftLight->AddComponent<LightComponent>();
+   Transform* leftLightTransform = leftLight->GetTransform();
+   leftLightComponent->SetLightType(ELightType::Point);
+   leftLightComponent->SetRadiance(Vector3(0.0f, 50.0f, 0.0f));
+   leftLightTransform->SetPosition(Vector3(-15.0f, 0.0f, 0.0f));
 
-   //auto light = world->CreateEntity( TEXT( "DirectionalLight" ) );
-   //auto lightComp = light->AddComponent<LightComponent>( );
-   //auto lightTransform = light->GetTransform( );
-   //lightTransform->SetPosition( Vector3( 0.0f, 0.0f, 0.0f ) );
-   ////lightTransform->SetRotation( Quaternion( 90.0f, Vector3::Up( ) ) );
-   //light->AddComponent<RotateComponent>( );
-   //// Light setup
+   Entity* rightLight = world->CreateEntity(TEXT("Right Light"));
+   LightComponent* rightLightComponent = rightLight->AddComponent<LightComponent>();
+   Transform* rightLightTransform = rightLight->GetTransform();
+   rightLightComponent->SetLightType(ELightType::Point);
+   rightLightComponent->SetRadiance(Vector3(0.0f, 0.0f, 50.0f));
+   rightLightTransform->SetPosition(Vector3(15.0f, 0.0f, 0.0f));
 
-   ////auto cube_model = resMng->Load<Model>( TEXT( "Contents/Models/cube.obj" ) );
-   //auto model = resMng->Load<Model>( TEXT( "Contents/Models/sphere.3ds" ) );
-   //auto diffuse = resMng->Load<Texture2D>( TEXT( "Contents/Textures/Pebbles_002_COLOR.jpg" ) );
-   ////auto diffuse = resMng->Load<Texture2D>( TEXT( "Contents/Textures/su0.png" ) );
-   //auto normal = resMng->Load<Texture2D>( TEXT( "Contents/Textures/Pebbles_002_NRM.jpg" ) );
-
-   //// Model setup
-
-   ///*Entity* modelEntity = Model::Instantiate( cube_model, world );
-   //Transform* modelTransform = modelEntity->GetTransform( );
-   //auto meshRenderer = modelEntity->GetChildren( )[ 0 ]->GetComponent< MeshRenderComponent >( );
-   //auto material = meshRenderer->GetMaterial( );
-   //material->SetDiffuseMap( diffuse );
-   //material->SetNormalMap( normal );
-   //modelTransform->SetPosition( Vector3( 0.0f, 0.0f, 0.0f ) );*/
-   ////modelEntity->AddComponent<RotateComponent>( );
-
-   //std::array<Entity*, 4> modelEntities{ 
-   //   Model::Instantiate( model, world ),
-   //   Model::Instantiate( model, world ) ,
-   //   Model::Instantiate( model, world ) ,
-   //   Model::Instantiate( model, world ) };
-
-   //std::array<Transform*, 4> transforms{
-   //   modelEntities[ 0 ]->GetTransform( ),
-   //   modelEntities[ 1 ]->GetTransform( ),
-   //   modelEntities[ 2 ]->GetTransform( ),
-   //   modelEntities[ 3 ]->GetTransform( )
-   //};
-
-   //std::array<MeshRenderComponent*, 4> modelRenderers{
-   //   modelEntities[ 0 ]->GetChildren( )[ 0 ]->GetComponent< MeshRenderComponent >( ),
-   //   modelEntities[ 1 ]->GetChildren( )[ 0 ]->GetComponent< MeshRenderComponent >( ),
-   //   modelEntities[ 2 ]->GetChildren( )[ 0 ]->GetComponent< MeshRenderComponent >( ),
-   //   modelEntities[ 3 ]->GetChildren( )[ 0 ]->GetComponent< MeshRenderComponent >( )
-   //};
-
-   //std::array<Material*, 4> materials{
-   //   modelRenderers[ 0 ]->GetMaterial( ),
-   //   modelRenderers[ 1 ]->GetMaterial( ),
-   //   modelRenderers[ 2 ]->GetMaterial( ),
-   //   modelRenderers[ 3 ]->GetMaterial( )
-   //};
-
-   //for ( auto mat : materials )
-   //{
-   //   mat->SetDiffuseMap( diffuse );
-   //   mat->SetNormalMap( normal );
-	  //mat->Save();
-   //}
-
-   //float scale = 0.04f;
-   //float posBegin = -150.0f;
-   //float posDelta = 100.0f;
-   //float rotBegin = -45.0f;
-   //float rotDelta = 22.5f;
-   //unsigned int t = 0;
-   //for ( auto transform : transforms )
-   //{
-   //   transform->SetScale( Vector3( 0.5f, 0.5f, 0.5f ) );
-   //   transform->SetRotation(
-   //      Quaternion( Math::DegreeToRadian( rotBegin + ( rotDelta * t ) ),
-   //      Vector3::Up( ) ), TransformSpace::World );
-   //   transform->SetPosition(
-   //      Vector3( posBegin + ( posDelta * t ), 0.0f, 30.0f ) );
-   //   ++t;
-   //}
-
-   //MELog( context, TEXT( "Test" ), ELogType::DEBUG, TEXT( "TEST MESSAGE~" ), true );
-
-   //modelEntities[ 0 ]->AddComponent<RotateComponent>( );
+   Model* damagedHelmetModel = resMng->Load<Model>(TEXT("Contents/Models/DamagedHelmet/DamagedHelmet.gltf"));
+   Entity* damagedHelmet = Model::Instantiate(damagedHelmetModel, world, TEXT("DamagedHelmet"));
+   Entity* damagedHelmetMesh = damagedHelmet->GetChildren()[0];
+   RotateComponent* damagedHelmetRotation = damagedHelmet->AddComponent<RotateComponent>();
+   MeshRenderComponent* renderComponent = damagedHelmetMesh->GetComponent<MeshRenderComponent>();
+   Material* damagedHelmetMaterial = renderComponent->GetMaterial();
+   damagedHelmetMaterial->Save();
 
    int execute = engine->Execute( );
-   world->Save();
+   //world->Save();
    SafeDelete( context );
    return execute;
 }
