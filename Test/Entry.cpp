d0aca93005f457d6912ced11fe1b514e738d5d5f
Yang Kyowon <kyowonhorang@gmail.com>
2020-03-27 17:19:16 +0900

    Slightly modify sample

--- Test/Entry.cpp
+++ Test/Entry.cpp
@@ -38,34 +38,43 @@ int main( )
    camComponent->SetFov(60.0f);
    camTransform->SetPosition(Vector3(0.0f, 0.0f, -5.0f));
 
-   Entity* upperLight = world->CreateEntity(TEXT("Upper Light"));
-   LightComponent* upperLightComponent = upperLight->AddComponent<LightComponent>();
-   Transform* upperLightTransform = upperLight->GetTransform();
-   upperLightComponent->SetLightType(ELightType::Point);
-   upperLightComponent->SetRadiance(Vector3(100.0f, 100.0f, 100.0f));
-   upperLightTransform->SetPosition(Vector3(0.0f, 10.0f, 0.0f));
-
-   Entity* leftLight = world->CreateEntity(TEXT("Left Light"));
-   LightComponent* leftLightComponent = leftLight->AddComponent<LightComponent>();
-   Transform* leftLightTransform = leftLight->GetTransform();
-   leftLightComponent->SetLightType(ELightType::Point);
-   leftLightComponent->SetRadiance(Vector3(0.0f, 50.0f, 0.0f));
-   leftLightTransform->SetPosition(Vector3(-15.0f, 0.0f, 0.0f));
-
-   Entity* rightLight = world->CreateEntity(TEXT("Right Light"));
-   LightComponent* rightLightComponent = rightLight->AddComponent<LightComponent>();
-   Transform* rightLightTransform = rightLight->GetTransform();
-   rightLightComponent->SetLightType(ELightType::Point);
-   rightLightComponent->SetRadiance(Vector3(0.0f, 0.0f, 50.0f));
-   rightLightTransform->SetPosition(Vector3(15.0f, 0.0f, 0.0f));
+   Entity* mainLight = world->CreateEntity(TEXT("Upper Light"));
+   LightComponent* mainLightComponent = mainLight->AddComponent<LightComponent>();
+   Transform* mainLightTransform = mainLight->GetTransform();
+   mainLightComponent->SetLightType(ELightType::Point);
+   mainLightComponent->SetRadiance(Vector3(200.0f, 200.0f, 200.0f));
+   mainLightTransform->SetPosition(Vector3(0.0f, 0.0f, -5.0f));
 
    Model* damagedHelmetModel = resMng->Load<Model>(TEXT("Contents/Models/DamagedHelmet/DamagedHelmet.gltf"));
    Entity* damagedHelmet = Model::Instantiate(damagedHelmetModel, world, TEXT("DamagedHelmet"));
    Entity* damagedHelmetMesh = damagedHelmet->GetChildren()[0];
    RotateComponent* damagedHelmetRotation = damagedHelmet->AddComponent<RotateComponent>();
-   MeshRenderComponent* renderComponent = damagedHelmetMesh->GetComponent<MeshRenderComponent>();
-   Material* damagedHelmetMaterial = renderComponent->GetMaterial();
+   MeshRenderComponent* helmetRenderComponent = damagedHelmetMesh->GetComponent<MeshRenderComponent>();
+   Material* damagedHelmetMaterial = helmetRenderComponent->GetMaterial();
    damagedHelmetMaterial->Save();
+
+   Model* lanternModel = resMng->Load<Model>(TEXT("Contents/Models/Lantern/Lantern.gltf"));
+   Entity* lantern = Model::Instantiate(lanternModel, world, TEXT("Lantern"));
+   Entity* lanternMesh = lantern->GetChildren()[0];
+   RotateComponent* lanternRotateComponent = lantern->AddComponent<RotateComponent>();
+   MeshRenderComponent* lanternRenderComponent = lanternMesh->GetComponent<MeshRenderComponent>();
+   Material* lanternMaterial = lanternRenderComponent->GetMaterial();
+   lanternMaterial->Save();
+
+   Transform* lanternTransform = lantern->GetTransform();
+   lanternTransform->SetScale(Vector3(0.1f, 0.1f, 0.1f));
+   lanternTransform->SetPosition(Vector3(-2.5f, -0.9f, 0.0f));
+
+   Model* metalRoughSpheresModel = resMng->Load<Model>(TEXT("Contents/Models/MetalRoughSpheres/MetalRoughSpheres.gltf"));
+   Entity* spheresEntity = Model::Instantiate(metalRoughSpheresModel, world, TEXT("Spheres"));
+   Entity* spheresMesh = spheresEntity->GetChildren()[0];
+   MeshRenderComponent* spheresRenderComponent = spheresMesh->GetComponent<MeshRenderComponent>();
+   Material* spheresMaterial = spheresRenderComponent->GetMaterial();
+   spheresMaterial->Save();
+
+   Transform* spheresTransform = spheresEntity->GetTransform();
+   //spheresTransform->SetScale(Vector3(0.1f, 0.1f, 0.1f));
+   spheresTransform->SetPosition(Vector3(0.0f, 0.0f, 4.0f));
 
    int execute = engine->Execute( );
    //world->Save();
