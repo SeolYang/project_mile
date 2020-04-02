db45f0afb16c63b5cdaec4a69ce5f1a96dfbb188
Yang Kyowon <kyowonhorang@gmail.com>
2020-04-02 12:54:44 +0900

    Add normal for Quad

--- Runtime/Rendering/Quad.cpp
+++ Runtime/Rendering/Quad.cpp
@@ -7,17 +7,17 @@ namespace Mile
    bool Quad::Init(float minX, float minY,
       float maxX, float maxY)
    {
-      std::vector<VertexPosTex> vertices = {
-      { Vector4(minX, maxY, 0.0f, 1.0f), Vector2(0.0f, 0.0f) },
-      { Vector4(maxX, maxY, 0.0f, 1.0f), Vector2(1.0f, 0.0f) },
-      { Vector4(minX, minY, 0.0f, 1.0f), Vector2(0.0f, 1.0f) },
-      { Vector4(maxX, minY, 0.0f, 1.0f), Vector2(1.0f, 1.0f) } };
+      std::vector<VertexPTN> vertices = {
+      { Vector4(minX, maxY, 0.0f, 1.0f), Vector2(0.0f, 0.0f), Vector3(0.0f, 0.0f, -1.0f) },
+      { Vector4(maxX, maxY, 0.0f, 1.0f), Vector2(1.0f, 0.0f), Vector3(0.0f, 0.0f, -1.0f) },
+      { Vector4(minX, minY, 0.0f, 1.0f), Vector2(0.0f, 1.0f), Vector3(0.0f, 0.0f, -1.0f) },
+      { Vector4(maxX, minY, 0.0f, 1.0f), Vector2(1.0f, 1.0f), Vector3(0.0f, 0.0f, -1.0f) } };
 
       std::vector<unsigned int> indices = {
          0, 1, 2,
          2, 1, 3
       };
 
-      return Mesh::Init<VertexPosTex>(vertices, indices);
+      return Mesh::Init<VertexPTN>(vertices, indices);
    }
 }
