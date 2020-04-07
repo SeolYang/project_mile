2c5c8cd0147272f33881c8a351cd8cb117024d99
Yang Kyowon <kyowonhorang@gmail.com>
2020-04-07 20:06:32 +0900

    Add comment on RenderObject ,and modify DynamicCubemap class comment

--- Runtime/Rendering/DynamicCubemap.h
+++ Runtime/Rendering/DynamicCubemap.h
@@ -5,7 +5,7 @@
 namespace Mile
 {
    /**
-    * @brief   Dynamic Cubemap은 렌더타겟(GPU Readable/Writable)으로 쓰일수있는 큐브맵이다.
+    * @brief   Dynamic Cubemap은 렌더타겟(GPU Readable/Writable)으로 쓰일수있는 큐브맵입니다.
     *          Face Index: (0, +X), (1, -X), (2, +Y), (3, -Y), (4, +Z), (5, -Z)
     * @format  (R16, G16, B16, A16)_UNORM
     *          
@@ -22,7 +22,7 @@ namespace Mile
 
       bool BindAsRenderTarget(ID3D11DeviceContext& deviceContext, unsigned int faceIdx, bool clearRenderTarget = true, bool clearDepth = true);
       /* 
-      * @warn  실제로 Cubemap이 렌더 타겟으로 바인드 되어있는지 여부를 따지지 않고 무조건 unbind 함.
+      * @warn  실제로 Cubemap이 렌더 타겟으로 바인드 되어있는지 여부를 따지지 않고 무조건 unbind 합니다.
       **/
       void UnbindAsRenderTarget(ID3D11DeviceContext& deviceContext);
 
--- Runtime/Rendering/RenderObject.h
+++ Runtime/Rendering/RenderObject.h
@@ -4,6 +4,12 @@
 
 namespace Mile
 {
+   /**
+    * @brief   렌더러를 가지고있는 오브젝트들의 공통 인터페이스.
+    *          이 클래스를 상속하는 자식 클래스들은 Init 함수를 구현할때 IsInitializable로 미리
+    *          초기화 가능한지 확인하여야 하며, 초기화가 완료되었을시 RenderObject::ConfirmInit() 메서드를 호출하여야 합니다.
+    *          Bind 함수를 구현할시에도 IsBindable 함수로, Bind 가능 여부를 체크하여야 합니다.
+    */
    class RendererDX11;
    class MEAPI RenderObject
    {
@@ -12,8 +18,23 @@ namespace Mile
       virtual ~RenderObject();
 
       FORCEINLINE bool IsInitialized() const { return m_bIsInitialized; }
+      /**
+       * @brief   현재 오브젝트가 소유중인(또는 오브젝트를 소유하는) 렌더러가 엔진에서 사용하고 있는 주 렌더러인지 확인 합니다.
+       */
       bool HasAvailableRenderer() const;
+
+      /**
+       * @brief   오브젝트를 초기할 수 있는지 체크합니다. 이미 초기화 되어있지 않아야하며, 
+       *          엔진의 주 렌더러와 오브젝트의 렌더러가 동일하여야 합니다.
+       */
       FORCEINLINE bool IsInitializable() const { return (!IsInitialized() && HasAvailableRenderer()); }
+
+      /**
+       * @brief   오브젝트를 바인드할 수 있는지 체크합니다. 초기화가 완료되어있어야 하며,
+       *          엔진의 주 렌더러와 오브젝트의 렌더러가 동일하여야 합니다.
+       *
+       * @warn    오브젝트가 실제로 파이프라인에 바인드되어 있는지 여부와는 관련이 없습니다.   
+       */
       FORCEINLINE bool IsBindable() const { return (IsInitialized() && HasAvailableRenderer()); }
       FORCEINLINE RendererDX11* GetRenderer() const { return m_renderer; }
 
