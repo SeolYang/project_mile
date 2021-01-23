a0cc9b8aad38d583b65640f9f7812d96d851a1a2
SeolYang <kyowonhorang@gmail.com>
2021-01-23 22:45:14 +0900

    GameViewLayer의 Window가 선택되었을때만 태두리를 표시하도록 하였습니다

--- Editor/Layers/GameViewLayer.cpp
+++ Editor/Layers/GameViewLayer.cpp
@@ -49,23 +49,26 @@ namespace Mile
          if (m_editorCameraRenderTex != nullptr)
          {
             auto contentRegion = ImGui::GetContentRegionAvail();
-            
+
+            if (ImGui::IsWindowFocused())
+            {
+               ImVec2 vMin;
+               ImVec2 vMax = ImGui::GetWindowSize();
+
+               ImVec2 winPos = ImGui::GetWindowPos();
+               vMin.x += winPos.x;
+               vMin.y += winPos.y;
+               vMax.x += winPos.x;
+               vMax.y += winPos.y;
+
+               ImGui::GetForegroundDrawList()->AddRect(vMin, vMax, IM_COL32(255, 255, 0, 255), 0.0f, 15, 0.8f);
+            }
+
             ImVec2 outputRes{ contentRegion.x, contentRegion.y };
             m_editorCameraRenderTex->SetWidth(outputRes.x);
             m_editorCameraRenderTex->SetHeight(outputRes.y);
             //m_editorCameraComponent->SetFov(GameViewDefaultFOV * ((outputRes.x + outputRes.y) / (GameViewDefaultWidth + GameViewDefaultHeight)));
             ImGui::Image((void*)m_editorCameraRenderTex->GetRenderTarget()->GetTexture()->GetSRV(), outputRes);
-
-            ImVec2 vMin = ImGui::GetWindowContentRegionMin();
-            ImVec2 vMax = ImGui::GetWindowContentRegionMax();
-
-            ImVec2 winPos = ImGui::GetWindowPos();
-            vMin.x += winPos.x;
-            vMin.y += winPos.y;
-            vMax.x += winPos.x;
-            vMax.y += winPos.y;
-
-            ImGui::GetForegroundDrawList()->AddRect(vMin, vMax,IM_COL32(255, 255, 0, 255), 0.0f, 15, 0.8f);
          }
          ImGui::End();
       }
