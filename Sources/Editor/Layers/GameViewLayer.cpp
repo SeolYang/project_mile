4c20bd760c94a624f2bd88cf7da711e97c01fe3a
SeolYang <kyowonhorang@gmail.com>
2021-02-18 01:24:32 +0900

    GameViewLayer가 다시 타겟 렌더링 해상도에 대응하도록 변경하였습니다

--- Sources/Editor/Layers/GameViewLayer.cpp
+++ Sources/Editor/Layers/GameViewLayer.cpp
@@ -41,6 +41,7 @@ namespace Mile
          ImGuiWindowFlags windowFlag = ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse;
          auto world = Engine::GetWorld();
          auto window = Engine::GetWindow();
+         auto renderer = Engine::GetRenderer();
 
          ImGui::PushStyleColor(ImGuiCol_ChildBg, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
          ImGui::Begin("Game", nullptr, windowFlag);
@@ -54,12 +55,11 @@ namespace Mile
                float titleBarHeight = ImGui::GetCurrentWindow()->TitleBarHeight();
                ImVec2 actualContentArea = ImVec2{ windowSize.x, windowSize.y - titleBarHeight };
 
-               auto renderTexWidth = (float)m_editorCameraRenderTex->GetWidth();
-               auto renderTexHeight = (float)m_editorCameraRenderTex->GetHeight();
-               Vector2 relativeOutputRes = FindResolutionWithAspectRatio(actualContentArea.x, actualContentArea.y, renderTexWidth/renderTexHeight);
+               auto renderRes = renderer->GetRenderResolution();
+               Vector2 relativeOutputRes = FindResolutionWithAspectRatio(actualContentArea.x, actualContentArea.y, renderRes.x / renderRes.y);
                ImVec2 outputRes{ relativeOutputRes.x, relativeOutputRes.y };
-               //m_editorCameraRenderTex->SetWidth((UINT32)referenceRes.x);
-               //m_editorCameraRenderTex->SetHeight((UINT32)referenceRes.y);
+               m_editorCameraRenderTex->SetWidth((UINT32)renderRes.x);
+               m_editorCameraRenderTex->SetHeight((UINT32)renderRes.y);
                //m_editorCameraComponent->SetFov(GameViewDefaultFOV * ((outputRes.x + outputRes.y) / (GameViewDefaultWidth + GameViewDefaultHeight)));
                ImGui::SetCursorPosX((actualContentArea.x - outputRes.x) * 0.5f);
                ImGui::SetCursorPosY((actualContentArea.y - outputRes.y) * 0.5f + titleBarHeight);
