ba2b9e44ec5dddec67a7b2f891db4ca9bd1d0069
SeolYang <kyowonhorang@gmail.com>
2021-02-19 22:45:22 +0900

    Optimized String convert utility function

--- Sources/Runtime/Core/CoreMinimal.h
+++ Sources/Runtime/Core/CoreMinimal.h
@@ -177,22 +177,23 @@ namespace Mile
 
    static String String2WString(const std::string& str)
    {
+      static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
       if (str.empty())
       {
-         return TEXT("");
+         return std::wstring();
       }
-      std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+
       return converter.from_bytes(str);
    }
 
    static std::string WString2String(const String& str)
    {
+      static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
       if (str.empty())
       {
          return std::string();
       }
 
-      std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
       return converter.to_bytes(str);
    }
 
