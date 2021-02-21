19b67314c380b25f4ac170885ab80062ad788e51
SeolYang <kyowonhorang@gmail.com>
2021-02-22 02:55:13 +0900

    빛에 관련된 enum이나 utility 함수 그리고 상수들을 정의할 light 헤더를 추가하였습니다

---
+++ Sources/Runtime/Rendering/Light.h
@@ -0,0 +1,75 @@
+#pragma once
+#include "Core/CoreMinimal.h"
+#include "Math/MathMinimal.h"
+
+namespace Mile
+{
+   constexpr float MaximumPossibleLuminousEfficacy = 683.0f;
+
+   enum class MEAPI ELightType : UINT32
+   {
+      Directional,
+      Point,
+      Spot
+   };
+
+   enum class MEAPI ELightIntensityUnit : UINT32
+   {
+      Lumen, /** lm */
+      Candela, /** cd (lm/sr) */
+      Lux, /** lx (lm/m^2) */
+      Nit /** nt (cd/m^2) */
+   };
+
+   inline ELightIntensityUnit LightIntensityUnitOf(const ELightType& lightType)
+   {
+      switch (lightType)
+      {
+      case ELightType::Directional:
+         return ELightIntensityUnit::Lux;
+      default:
+      case ELightType::Point:
+      case ELightType::Spot:
+         return ELightIntensityUnit::Lumen;
+      }
+   }
+
+   inline std::string LightIntensityUnitToString(const ELightIntensityUnit& unit, bool bAsSymbol = false)
+   {
+      if (bAsSymbol)
+      {
+         switch (unit)
+         {
+         case ELightIntensityUnit::Lumen:
+            return std::string("lm");
+         case ELightIntensityUnit::Candela:
+            return std::string("cd");
+         case ELightIntensityUnit::Lux:
+            return std::string("lx");
+         case ELightIntensityUnit::Nit:
+            return std::string("nt");
+         }
+      }
+      else
+      {
+         switch (unit)
+         {
+         case ELightIntensityUnit::Lumen:
+            return std::string("Lumen");
+         case ELightIntensityUnit::Candela:
+            return std::string("Candela");
+         case ELightIntensityUnit::Lux:
+            return std::string("Lux");
+         case ELightIntensityUnit::Nit:
+            return std::string("Nit");
+         }
+      }
+
+      return std::string();
+   }
+
+   inline float WattsToLumens(float Watt, float efficiency)
+   {
+      return Watt * (MaximumPossibleLuminousEfficacy * efficiency);
+   }
+}
